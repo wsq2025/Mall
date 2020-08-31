e07412d3f87802f7da2a00fbd67144754a074e06
wsq <673372988@qq.com>
2020-08-31 21:51:56 +0800

    项目初始化

---
+++ mall-product/src/test/java/com/future/newmall/product/MallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.future.newmall.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
